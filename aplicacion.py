4e65c793edca079934eef97787788cc250cf6e98
Aggasth <101593156+Aggasth@users.noreply.github.com>
2023-08-14 13:09:12 -0400

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 import datetime
+import random
+import names
 
 # Conexión a la base de datos MongoDB
 client = MongoClient('localhost', 27017)
@@ -14,11 +16,28 @@ def agregar_trabajador(nombre, fecha_nacimiento, sueldo):
         'fecha_registro': datetime.datetime.now()
     }
     collection.insert_one(trabajador)
-    print("Trabajador agregado exitosamente.")
+
+def generar_fecha_nacimiento():
+    year = random.randint(1970, 2000)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)  # Considerando febrero
+    return f"{year:04d}-{month:02d}-{day:02d}"
+
+def generar_sueldo():
+    return round(random.uniform(1500, 5000), 2)
 
 if __name__ == "__main__":
-    nombre = input("Ingrese el nombre del trabajador: ")
-    fecha_nacimiento = input("Ingrese la fecha de nacimiento (YYYY-MM-DD): ")
-    sueldo = float(input("Ingrese el sueldo del trabajador: "))
+    for _ in range(3):
+        nombre = names.get_full_name()
+        fecha_nacimiento = generar_fecha_nacimiento()
+        sueldo = generar_sueldo()
+        
+        agregar_trabajador(nombre, fecha_nacimiento, sueldo)
     
-    agregar_trabajador(nombre, fecha_nacimiento, sueldo)
+    # Imprimir los trabajadores almacenados en la base de datos
+    print("Trabajadores almacenados en la base de datos:")
+    for trabajador in collection.find():
+        print(f"Nombre: {trabajador['nombre']}")
+        print(f"Fecha de Nacimiento: {trabajador['fecha_nacimiento']}")
+        print(f"Sueldo: {trabajador['sueldo']}")
+        print("---------------------------")
